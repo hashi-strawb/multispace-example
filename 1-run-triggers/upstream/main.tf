2773800c94ee48fc074138f35ca5ce9c5efe0a68
Lucy Davinhart <lucy@lucymhdavies.com>
2023-06-16 17:56:25 +0100

    sample upstream workspace

---
+++ 1-run-triggers/upstream/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  cloud {
+    organization = "fancycorp"
+
+    workspaces {
+      name = "1-upstream"
+    }
+  }
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+resource "random_pet" "example" {
+  length    = 1
+  prefix    = terraform.workspace
+  separator = ":"
+}
+
+output "random_pet" {
+  value = random_pet.example.id
+}
+
