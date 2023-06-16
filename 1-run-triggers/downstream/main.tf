284af8b40bb67796ee71616dadb42d5527879fc3
Lucy Davinhart <lucy@lucymhdavies.com>
2023-06-16 18:24:31 +0100

    example downstream workspace

---
+++ 1-run-triggers/downstream/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  cloud {
+    organization = "fancycorp"
+
+    workspaces {
+      tags = ["multispace:downstream"]
+    }
+  }
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0.0"
+    }
+
+    tfe = {
+      source  = "hashicorp/tfe"
+      version = ">= 0.45.0"
+    }
+  }
+}
+
+variable "upstream_workspaces" {
+  type    = set(string)
+  default = ["1-upstream"]
+}
+
+data "tfe_outputs" "upstream" {
+  for_each = var.upstream_workspaces
+
+  organization = "fancycorp"
+  workspace    = each.key
+}
+
+resource "random_pet" "example" {
+  length    = 1
+  prefix    = terraform.workspace
+  separator = ":"
+}
+
+output "random_pet" {
+  value = {
+    for upstream in var.upstream_workspaces :
+    data.tfe_outputs.upstream[upstream].nonsensitive_values.random_pet => random_pet.example.id
+  }
+}
+
