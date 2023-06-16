b5ed17fda32863fe07ac3436365487298f3337ff
Lucy Davinhart <lucy@lucymhdavies.com>
2023-06-16 21:52:45 +0100

    add a tfe_workspace_run to destroy the example workspace manager
    bit meta, innit?

--- 0-bootstrap/2-workspace-manager.tf
+++ 0-bootstrap/2-workspace-manager.tf
@@ -28,3 +28,25 @@ resource "tfe_variable" "ws-manager-upstream-tfc_org" {
   value        = var.tfc_org
   workspace_id = tfe_workspace.ws-manager-upstream.id
 }
+
+
+resource "tfe_workspace_run" "destroy-ws-manager-upstream" {
+  workspace_id = tfe_workspace.ws-manager-upstream.id
+
+  depends_on = [
+    # The workspace needs the tfc_org variable
+    tfe_variable.ws-manager-upstream-tfc_org,
+
+    # TFC creds need to exist, and be mapped to this workspace
+    tfe_variable.tfc-creds,
+    tfe_workspace_variable_set.tfc-creds,
+  ]
+
+  # So we can safely "terraform destroy" our bootstrap workspace...
+  destroy {
+    # Wait for destroy before doing anything else
+    wait_for_run = true
+    # auto-apply
+    manual_confirm = false
+  }
+}
